fd1cce63b5f5acd240e740dc5f1923bee5f12fe1
William O'Hanley <liam@wohanley.com>
2015-02-08 12:40:27 +1300

    pull signpost dependency

--- build.sbt
+++ build.sbt
@@ -12,5 +12,8 @@ libraryDependencies ++= Seq("org.specs2" %% "specs2-core" % "2.4.15" % "test")
 scalacOptions in Test ++= Seq("-Yrangepos")
 resolvers ++= Seq("snapshots", "releases").map(Resolver.sonatypeRepo)
 
-// Heroku stuff
-libraryDependencies ++= Seq("com.twitter" % "finagle-http_2.11" % "6.24.0")
+// Web stuff
+libraryDependencies ++= Seq(
+  "com.twitter" % "finagle-http_2.11" % "6.24.0",
+  "oauth.signpost" % "signpost-core" % "1.2"
+)
--- src/main/scala/animate.scala
+++ src/main/scala/animate.scala
@@ -4,14 +4,43 @@ import com.twitter.finagle.{Http, Service}
 import com.twitter.util.{Await, Future}
 import com.twitter.finagle.http.Response
 import java.net.InetSocketAddress
+import java.util.Timer
+import java.util.TimerTask
 import org.jboss.netty.handler.codec.http._
 import util.Properties
+import oauth.signpost.OAuth
+import oauth.signpost.OAuthConsumer
+import oauth.signpost.OAuthProvider
+import oauth.signpost.basic.DefaultOAuthConsumer
+import oauth.signpost.basic.DefaultOAuthProvider
 
 object Animator {
   def main(args: Array[String]) {
     val port = Properties.envOrElse("PORT", "8080").toInt
     val server = Http.serve(":" + port, new HelloService)
     Await.ready(server)
+
+    tweetRegularly()
+  }
+
+  def tweetRegularly() =
+    new Timer().schedule(TweetTask, 0, 15 * 60 * 1000)
+}
+
+object TweetTask extends TimerTask {
+
+  val wordMap = oracle.buildWordMap(List("fortunes", "wisdom", "goedel"))
+
+  /** Tweet a fortune */
+  override def run = {
+    val oauthConsumer = new DefaultOAuthConsumer(
+      "9JqqFKjWiA435oCGU9yuuM1MY",
+      Properties.envOrElse("API_SECRET", ""))
+    oauthConsumer.setTokenWithSecret(
+      "3012148274-YO67BWOFQHyQT0ff7DXPrsLtaemFsxSCfF8hmci",
+      Properties.envOrElse("ACCESS_TOKEN_SECRET", "")
+    )
+    
   }
 }
 
